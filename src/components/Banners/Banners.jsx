203b4d761300c649ebb02509f8a3e71dc1c25c87
Munna8383 <jahedmunna612@gmail.com>
2024-09-27 01:07:49 +0600

    fix banner

--- src/components/Banners/Banners.jsx
+++ src/components/Banners/Banners.jsx
@@ -7,9 +7,9 @@ const Banners = () => {
     return (
         <div>
 
-            <div className="bg-[url('https://i.imghippo.com/files/VTv3i1726517639.jpg')] bg-cover bg-center h-screen rounded-b-[-400px] ">
+            <div className="bg-[url('https://i.imghippo.com/files/Tb8k61727376165.jpg')] bg-cover bg-fixed bg-center h-screen rounded-b-[450px] ">
 
-                <div className="h-screen flex justify-center items-center bg-[rgba(0,0,0,0.6)] rounded-b-[-400px]">
+                <div className="h-screen flex justify-center items-center bg-[rgba(0,0,0,0.8)] rounded-b-[450px]">
 
                     <div className="space-y-10 text-white text-center sm:w-9/12 mx-auto z-40">
                         <h1 data-aos="fade-up" className="text-lg sm:text-4xl text-white font-bold">Your Path to Success Starts with the Right Tutor</h1>
